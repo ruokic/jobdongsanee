5b948fbff80fbb0e2adb535fe19fdc02bb5dffd6
ruokic <gusqo327@naver.com>
2024-09-06 08:58:30 +0900

    [feature] set default layout in home
    - add `Banner.tsx` in `app/ui/home/`
    - add `ContentBox.tsx` in `app/ui/home/`
    - set dummy in `app/page.tsx`

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import ContentBox from './ui/home/ContentBox';
+import Banner from './ui/home/Banner';
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className='flex flex-col gap-4'>
+      <Banner />
+      <div className='p-4 flex flex-wrap gap-4 justify-center'>
+        <ContentBox />
+        <ContentBox />
+        <ContentBox />
+        <ContentBox />
+      </div>
+    </main>
+  );
 }
