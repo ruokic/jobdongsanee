e9816c4363a1beb58eff818296150e8327c571a4
ruokic <gusqo327@naver.com>
2024-09-11 08:58:38 +0900

    [feature] add style in lottery
    - add `LotteryNumberItem` to set style in `app/lottery/page`
    - add default button component

--- app/lottery/page.tsx
+++ app/lottery/page.tsx
@@ -1,7 +1,38 @@
 'use client';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 import { getSixInFortyFive } from '../lib/lottery';
+
+const bgColorOfNumber = (number) => {
+  if (number < 10) return 'bg-yellow-400';
+  if (number < 20) return 'bg-sky-400';
+  if (number < 30) return 'bg-red-500';
+  if (number < 40) return 'bg-gray-400';
+  return 'bg-green-400';
+};
+
+function LotteryNumberItem({ number }) {
+  const bgColor = bgColorOfNumber(number);
+  return (
+    <span
+      className={`w-8 h-8 text-center rounded-full text-white leading-8 shadow-lg ${bgColor}`}
+    >
+      {number}
+    </span>
+  );
+}
+
+function Button({ text, onClick, ...props }) {
+  return (
+    <button
+      className='px-4 py-2 text-white bg-blue-400 rounded active:bg-blue-600'
+      onClick={onClick}
+      {...props}
+    >
+      {text}
+    </button>
+  );
+}
 
 export default function Lottery() {
   const [lotteryNumberArray, setLotteryNumberArray] = useState<
@@ -16,20 +47,23 @@ export default function Lottery() {
   };
 
   return (
-    <div className='w-60 flex flex-col p-4 items-center gap-4'>
+    <div className='flex flex-col p-4 items-center gap-4'>
       <h2 className='text-xl'>로또 번호 추첨기</h2>
-      <div className='flex justify-between w-full'>
-        <button onClick={handleDeleteAll}>초기화</button>
-        <button onClick={handleGenerate}>생성</button>
+      <div className='w-60 flex justify-between'>
+        <Button text='초기화' onClick={handleDeleteAll} />
+        <Button text='생성' onClick={handleGenerate} />
       </div>
-      <div className='w-full'>
+      <div className='flex flex-col gap-2'>
         {lotteryNumberArray.map((lotteryNumber) => (
           <div
             key={lotteryNumber.join('')}
-            className='grid grid-cols-6 text-center gap-1'
+            className='grid grid-cols-6 text-center gap-2'
           >
             {lotteryNumber.map((number) => (
-              <span key={lotteryNumber.join('') + number}>{number}</span>
+              <LotteryNumberItem
+                key={lotteryNumber.join('') + number}
+                number={number}
+              />
             ))}
           </div>
         ))}
