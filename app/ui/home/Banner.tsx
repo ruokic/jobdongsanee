7fff9a39b234462e25a0546b1e87fb792ca40123
ruokic <gusqo327@naver.com>
2024-09-14 20:22:16 +0900

    [chore] modify Banner
    - modify message in `Banner`

--- app/ui/home/Banner.tsx
+++ app/ui/home/Banner.tsx
@@ -1,9 +1,7 @@
 export default function Banner() {
   return (
     <div className='w-full h-64 flex justify-center text-center items-center shadow'>
-      <div className='p-4'>
-        제목은 잡동사니라고 하겠습니다... 근데 쓸데없는 것들을 곁들인...
-      </div>
+      <div className='p-4'>여기에 광고가 있었으면 좋겠다...</div>
     </div>
   );
 }
