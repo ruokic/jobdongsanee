c0ad3a37e775859c061b8e43054fc12238a27778
ruokic <gusqo327@naver.com>
2024-09-15 17:54:33 +0900

    [feature] add icons and modify css
    - add icons in `home`
    - modify css in `Modal` to prevent scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client';
 import ContentBox from './ui/home/ContentBox';
 import Banner from './ui/home/Banner';
 
+import CloverIcon from '../public/icons/clover.svg';
+import UtensilsIcon from '../public/icons/utensils.svg';
+
 const data = [
-  { title: '로또 번호 추첨', url: '/lottery' },
-  { title: '메뉴 추천', url: '/omakase' },
+  { title: '로또 번호 추첨', url: '/lottery', icon: CloverIcon },
+  { title: '메뉴 추천', url: '/omakase', icon: UtensilsIcon },
 ];
 
 export default function Home() {
@@ -11,8 +15,8 @@ export default function Home() {
     <>
       <Banner />
       <div className='p-4 flex flex-wrap gap-4 justify-center'>
-        {data.map(({ title, url }) => (
-          <ContentBox key={url} title={title} url={url} />
+        {data.map(({ title, url, icon }) => (
+          <ContentBox key={url} title={title} url={url} Icon={icon} />
         ))}
       </div>
     </>
--- app/ui/home/ContentBox.tsx
+++ app/ui/home/ContentBox.tsx
@@ -5,15 +5,17 @@ import { useRouter } from 'next/navigation';
 interface IContentBox {
   title: string;
   url: string;
+  Icon: React.ReactNode<SVGSVGElement>;
 }
 
-export default function ContentBox({ title, url }: IContentBox) {
+export default function ContentBox({ title, url, Icon }: IContentBox) {
   const router = useRouter();
   return (
     <button
-      className='h-32 w-32 flex flex-col justify-center items-center border rounded-lg active:bg-neutral-100'
+      className='h-32 w-32 flex flex-col justify-center items-center border rounded gap-4 active:bg-neutral-100'
       onClick={() => router.push(url)}
     >
+      <Icon className='w-8 h-8' />
       {title}
     </button>
   );
